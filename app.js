5f811cff63023810097e0783cba678023a17a01e
胡子超 <huzichao@gis580.com>
2022-05-17 13:08:14 +0800

    last

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 
+const { json } = require("express")
 const express = require('express')
 const {createServer}  =  require('http')
 const {Server}  = require('socket.io')
@@ -19,43 +20,45 @@ const STATE_SPEAK = 2; // 发言
 
 
 let count = 0
-
+let username = ''
+let headImage = ''
 io.on('connection',(socket)=>{
   count++
   io.emit('count',count)
   socket.on('username',(data)=>{
     socket.username = data
+    username = data
     io.emit('somebodyEnter',{
       username:socket.username,
       state:STATE_ENTER,
     })
   })
-
   // 获取用户的头像
-  socket.on("sendHeadImg", (data) => {
+  socket.on("sendHeadImg", (data)=>{
     socket.headImage = data;
+    headImage = data
   })
   socket.on('speak',(data)=>{
     // 广播
+      console.log(data);
+      let obj = JSON.parse(data)
 
-    // console.log(socket);
-    // console.log(socket.username);
     io.emit("somebodySpeak", {
-      username: socket.username,
-      speak: data,
-      headImage: socket.headImage,
+      username:obj.username,
+      headImgUrl:obj.headImgUrl,
+      msg: obj.msg,
       state: STATE_SPEAK,
       tm:socket.handshake.time
     });
   })
   // 断开连接
   socket.on('disconnect', function(){
-    count--;
-
+    console.log(11111);
     io.emit('somebodyLeave',{
       username:socket.username,
       state:STATE_LEAVE,
     })
+    count--;
     io.emit("count", count);
   });
   
